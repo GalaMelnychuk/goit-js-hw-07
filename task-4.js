2813a56a62c1bdc915241d3822f441acfd859148
Галина <galocka@MacBook-Galina.local>
2020-02-09 05:30:24 +0200

    task-4

---
+++ task-4.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
+
+// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
+// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+
+const buttonDecrement = document.querySelector('[data-action="decrement"]');
+
+const buttonIncrement = document.querySelector('[data-action="increment"]');
+
+const clickValue = document.querySelector("#value");
+let counterValue = 0;
+
+function handleClickDecrement(event) {
+  clickValue.textContent = counterValue -= 1;
+  return buttonDecrement.textContent;
+}
+function handleClickIncrement(event) {
+  clickValue.textContent = counterValue += 1;
+  return buttonDecrement.textContent;
+}
+
+buttonDecrement.addEventListener("click", handleClickDecrement);
+
+buttonIncrement.addEventListener("click", handleClickIncrement);
