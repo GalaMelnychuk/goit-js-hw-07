8a4d159903827dfaf74cd1442c0d4c949d131cb9
Галина <galocka@MacBook-Galina.local>
2020-02-09 17:30:28 +0200

    task-5

---
+++ task-5.js
@@ -0,0 +1,16 @@
+"use strict";
+// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+
+const nameInput = document.querySelector("#name-input");
+const nameOuput = document.querySelector("#name-output");
+
+function handleInput(event) {
+  //   const input = event.currentTarget;
+  if (nameInput.value === "") {
+    nameOuput.textContent = "незнакомец";
+  } else {
+    nameOuput.textContent = nameInput.value;
+  }
+}
+
+nameInput.addEventListener("input", handleInput);
