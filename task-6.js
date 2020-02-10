6202da9099fbf4567e8775d68ab7587478726036
Галина <galocka@MacBook-Galina.local>
2020-02-10 14:04:16 +0200

    task-7

--- task-6.js
+++ task-6.js
@@ -23,12 +23,12 @@ input.addEventListener("change", handleInput);
 
 // Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input) и изменяет инлайн-стиль span#text обновляя свойство font-size. В результате при перетаскивании ползунка будет меняться размер текста.
 
-const text = document.querySelector("#text");
-const fontSizeControl = document.querySelector("#font-size-control");
-console.log(fontSizeControl.style.fontSize);
+// const text = document.querySelector("#text");
+// const fontSizeControl = document.querySelector("#font-size-control");
+// console.log(fontSizeControl.style.fontSize);
 
-function rangeFontSize(event) {
-  text.style.fontSize = fontSizeControl.value + "px";
-}
+// function rangeFontSize(event) {
+//   text.style.fontSize = fontSizeControl.value + "px";
+// }
 
-fontSizeControl.addEventListener("input", rangeFontSize);
+// fontSizeControl.addEventListener("input", rangeFontSize);
